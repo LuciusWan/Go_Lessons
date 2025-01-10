75a930d704f301d127130472e136a71c8820e429
LuciusWan <18099488938@163.com>
2025-01-11 00:13:17 +0800

    导包函数运用 README.md

---
+++ function/func1.go
@@ -0,0 +1,6 @@
+package function
+
+func Func1(a int, b int) (c int) {
+	c = a + b
+	return c
+}
--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"awesomeProject1/function"
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	fmt.Println(" Hello World")
+	fmt.Println(function.Func1(1, 2))
 }
 
 //TIP See GoLand help at <a href="https://www.jetbrains.com/help/go/">jetbrains.com/help/go/</a>.
